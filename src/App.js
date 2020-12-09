c33e6739c738c6ec48e914e872a5cd38a512d753
urbano <urbanobaz@yahoo.com>
2020-12-08 23:23:46 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import Rock from './icons/Rock';
+import Paper from './icons/Paper';
+import Scissors from './icons/Scissors';
+import './App.css';
+
+const choices = [
+  { id: 1, name: 'rock', component: Rock },
+  { id: 2, name: 'paper', component: Paper },
+  { id: 3, name: 'scissors', component: Scissors },
+];
+
+export default function App() {
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+
+  useEffect(() => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(randomChoice);
+  }, []);
+
+  function handleUserChoice(choice) {
+    const chosenChoice = choices.find((c) => c.id === choice);
+    setUserChoice(chosenChoice);
+  }
+
+  return (
+    <div className="app">
+      {/* information goes here */}
+      <div className="info">
+        <h2>Rock. Paper. Scissors</h2>
+
+        {/* wins vs losses stats */}
+        <div className="wins-losses">
+          <div className="wins">
+            <span className="number">0</span>
+            <span className="text">Wins</span>
+          </div>
+
+          <div className="losses">
+            <span className="number">0</span>
+            <span className="text">Losses</span>
+          </div>
+        </div>
+      </div>
+
+      {/* the popup to show win/loss/draw */}
+      {/* <div className="game-state"></div> */}
+
+      <div className="choices">
+        {/* choices captions */}
+        <div>You</div>
+        <div />
+        <div>Computer</div>
+
+        {/* buttons for my choice */}
+        <div>
+          <button className="rock" onClick={() => handleUserChoice(1)}>
+            <Rock />
+          </button>
+          <button className="paper" onClick={() => handleUserChoice(2)}>
+            <Paper />
+          </button>
+          <button className="scissors" onClick={() => handleUserChoice(3)}>
+            <Scissors />
+          </button>
+        </div>
+
+        <div className="vs">vs</div>
+
+        {/* show the computer's choice */}
+        <div>
+          <button className="computer-choice">?</button>
+        </div>
+      </div>
+    </div>
+  );
+}
